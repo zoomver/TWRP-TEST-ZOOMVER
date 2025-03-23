81ccd6e5c91c5aa1d9c9299b41fca0c7250a3bf8
zoomver <vyvodnovprestigio@gmail.com>
2025-03-23 21:37:43 +0500

    ZTE TEST

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # TWRP-TEST-ZOOMVER
+0. Первичная сборка прошла на 85% (почему-то?) (twrp запустился).GitHub написал, что сборка закончена и дал ссылку на скачивание (выставлены галочки в тёмных кругах).
+1. Протестировал twrp, провожу работу над ошибками.
+2. Заменил twrp.fstab (от донора).
+3. Отредактировал twrp.fstab
+4. Добавил в BoardConfig флаги:
+Storage
+TW_INTERNAL_STORAGE_PATH := "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+5.   Сделал тестовую сборку. Сборка прошла на 100% (почему-то?).
+GitHub написал, что сборка закончена и дал ссылку на скачивание (выставлены галочки в белых кругах).
+6. Добавил в BoarConfig флаги:
+TW_INTERNAL_STORAGE_PATH := "/storage/emulated/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
+TW_EXTERNAL_STORAGE_PATH := "/storage/sdcard0"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+7. Добавил vendor.prop
+8. Сделал тестовую сборку.
+9. Отредактировал twrp.fstab
+10. Сделал тестовую сборку.
+11. Тестовая сборка в купе со снятием шифрования (редактировал fstab vendor) заработала. Необходимо править пути монтирования Data, sdcard, cache,odmko, ztepersist, metadata, Убрать super+,дубликат Data, cache.
+12. Отредактировал флаги в BoardConfig:
+TW_INTERNAL_STORAGE_PATH := "/storage/emulated/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "/storage"
+TW_EXTERNAL_STORAGE_PATH := "/storage/sdcard0"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "/storage"
+13.  
---
+++ device.mk
@@ -0,0 +1,66 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+LOCAL_PATH := device/zte/P963F70
+
+PRODUCT_PLATFORM := sp9863a
+
+# Dynamic Partitions stuff
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# VNDK
+PRODUCT_TARGET_VNDK_VERSION := 30
+
+# API
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# A/B
+PRODUCT_PROPERTY_OVERRIDES += ro.apex.updatable=false
+
+# Hidl Service
+PRODUCT_ENFORCE_VINTF_MANIFEST := true
+
+# A/B
+ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# Health Hal
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
+
+# Boot control HAL
+PRODUCT_PACKAGES += \
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-impl.recovery \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.$(PRODUCT_PLATFORM) \
+    bootctrl.$(PRODUCT_PLATFORM).recovery
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    android.hardware.fastboot@1.0-impl-mock.recovery \
+    fastbootd
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
+
+PRODUCT_PACKAGES += \
+    otapreopt_script \
+    cppreopts.sh \
+    update_engine \
+    update_verifier \
+    update_engine_sideload
