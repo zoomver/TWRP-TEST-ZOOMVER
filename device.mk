a3db951219f3d976b6c0e140ab06d12b0632b9e0
zoomver <119610499+zoomver@users.noreply.github.com>
2025-04-23 10:50:41 +0500

    Update device.mk

--- device.mk
+++ device.mk
@@ -20,6 +20,7 @@ PRODUCT_SHIPPING_API_LEVEL := 30
 
 # Virtual A/B
 ENABLE_VIRTUAL_AB := true
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 OVERRIDE_TARGET_FLATTEN_APEX := true
 PRODUCT_PROPERTY_OVERRIDES += ro.apex.updatable=true
